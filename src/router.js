d52de5b0328d866b1a3ef21b1d3a9ac2bec47a4f
wuyp <wuyp@fsmeeting.com>
2020-08-14 16:15:49 +0800

    部署环境设置

--- src/router.js
+++ src/router.js
@@ -4,8 +4,11 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 import Main from './components/main/main'
+console.log(process.env.NODE_ENV)
 
 export default new Router({
+    mode: 'history',
+    // base: '/mc_mobile',
     routes: [
         {
             path: '/', name: 'home', component: Main
